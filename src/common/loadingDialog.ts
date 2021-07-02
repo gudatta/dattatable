b4108d38ec23e8e16bf85ad5e75e57748c8fa383
Gunjan Datta <me@dattabase.com>
2021-07-02 11:49:00 -0400

    Added a loading dialog.

---
+++ src/common/loadingDialog.ts
@@ -0,0 +1,13 @@
+import { Modal } from "./modal";
+
+/**
+ * Loading Dialog
+ */
+export class LoadingDialog {
+    // Hides the loading dialog
+    static hide() {}
+
+    // Shows the loading dialog
+    static show(message:string) {
+    }    
+}
