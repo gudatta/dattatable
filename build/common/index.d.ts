c8a1e2f12ab243bc5e41594c92d75d9bc3926bca
Gunjan Datta <me@dattabase.com>
2021-07-02 09:02:45 -0400

    Removed the build directory.

--- build/common/index.d.ts
+++
@@ -1,6 +0,0 @@
-/** Libraries */
-export * from "./canvas";
-export * from "./modal";
-/** Methods */
-export declare const formatDateValue: (value: string) => string;
-export declare const formatDateTimeValue: (value: string) => string;
