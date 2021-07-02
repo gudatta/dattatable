0a42ca79b0b23e9eb95da6ddc22531e01efe1599
Gunjan Datta <me@dattabase.com>
2021-07-02 16:36:48 -0400

    Made the external libraries available.

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,11 @@ export * from "./itemForm";
 
 /** Styling */
 import "./styles";
+
+/** External Librarys */
+export * from "gd-sprest-bs";
+import * as $ from "jquery";
+import * as moment from "moment";
+export {
+    $, moment
+}
